0addb88002debc331ef8d5a6f278b4118581e4ce
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-08-06 17:18:40 +0530

    Hi

---
+++ Session1Assignment1.java
@@ -0,0 +1,11 @@
+public class Session1Assignment1
+{
+    public static void main(String [] args)
+    {
+            int num1 = 10;
+            float num2 = 2;
+            int sum =  num1 +  (int) num2;               
+            System.out.println("Sum of two numbers is : "+ sum);
+     }
+
+}
